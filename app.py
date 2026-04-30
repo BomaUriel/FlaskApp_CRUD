938918584356a4ed9dd13f013b11ea48d07ff979
BOMAURIEL <BOMAURIEL@YAHOO.COM>
2026-04-29 23:28:17 -0500

    Initial commit: Set up Flask application with database, templates, and styles

---
+++ app.py
@@ -0,0 +1,58 @@
+# Imports
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+# Main app
+app = Flask(__name__)
+
+app.config['SCSS_ASSET_DIR'] = 'static'
+Scss(app)
+
+
+# Database Config
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data Class ~ Row of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"Task {self.id}"
+
+
+
+# Routes to Webpages
+@app.route("/", methods=["POST","GET"])
+def index():
+
+    #Add Task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
+    # See all the the current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', tasks=tasks)
+    
+
+
+
+# Runner and Debugger
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
